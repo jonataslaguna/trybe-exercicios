29a5298c8b41f77be9ffd5b92e3e9e0e1762e531
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-03-28 10:39:53 -0300

    exercicio 02 3.1

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-01-javascript-primeiros-passos/ex02.js
@@ -0,0 +1,9 @@
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientInfo)
