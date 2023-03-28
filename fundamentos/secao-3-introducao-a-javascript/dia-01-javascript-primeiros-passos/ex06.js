0a322aae4808ae1048e60c8fdd78f6c56246ddf4
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-03-28 12:06:26 -0300

    exercicio06 3.1

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-01-javascript-primeiros-passos/ex06.js
@@ -0,0 +1,7 @@
+let weekDay = 'sabado'
+
+if(weekDay == 'segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira'){
+    console.log('Dia de estudo!')
+}else{
+    console.log('FINALMENTE, descanso merecido!')
+}
