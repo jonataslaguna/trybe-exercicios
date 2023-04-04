63bf8801083bc692810b9619197dbfa00eaf4721
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-04-04 11:05:08 -0300

    exercicio06-3.5

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-05-let-const-arrow-functions-e-template-literals/ex06.js
@@ -0,0 +1,11 @@
+/* function hello(nome) {
+    return `Olá, ${nome}!`
+  }
+  let nome = 'Ivan';
+  console.log(hello(nome)); */
+
+  const hello = (nome) => {
+    return `Olá, ${nome}!`
+  }
+  let nome = 'Ivan';
+  console.log(hello(nome));
