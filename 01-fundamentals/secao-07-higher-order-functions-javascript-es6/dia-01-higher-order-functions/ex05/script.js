ec0d67b5898df047825b2296af5cff39d37d09d7
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-05-05 15:33:34 -0300

    exercise-05/7.1

---
+++ 01-fundamentals/secao-07-higher-order-functions-javascript-es6/dia-01-higher-order-functions/ex05/script.js
@@ -0,0 +1,16 @@
+const newEmployees = (employeeGenerator) => {
+    const employees = {
+      id1: employeeGenerator('Pedro Guerra'),
+      id2: employeeGenerator('Luiza Drumond'), 
+      id3: employeeGenerator('Carla Paiva'), 
+    }
+    return employees;
+  };
+
+  const employeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_')
+    return {fullName, email: `${email}@betrybe.com`}
+    
+  }
+
+  console.log(newEmployees(employeeGenerator))
