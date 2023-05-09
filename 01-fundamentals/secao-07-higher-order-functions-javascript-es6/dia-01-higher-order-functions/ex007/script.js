0d769778f1792999c07379479298386d230ad71e
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-05-09 10:50:32 -0300

    exercise-07/7.1

--- 01-fundamentals/secao-07-higher-order-functions-javascript-es6/dia-01-higher-order-functions/ex007/script.js
+++ 01-fundamentals/secao-07-higher-order-functions-javascript-es6/dia-01-higher-order-functions/ex007/script.js
@@ -66,4 +66,16 @@ const author1947 = () => {
     return author
 }
 
-console.log(author1947())
+const expectedResult = 'Duna';
+const smallerName = () => {
+  let nameBook;
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name
+    }
+    
+  })
+  return nameBook;
+}
+
+console.log(smallerName())
