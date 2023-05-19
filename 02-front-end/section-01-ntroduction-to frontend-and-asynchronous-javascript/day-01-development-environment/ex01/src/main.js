98bc8df7ed2b9ee9a97801af8db531aca623ef8d
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-05-19 12:32:54 -0300

    exercise-01/1.1

--- 02-front-end/section-01-ntroduction-to frontend-and-asynchronous-javascript/day-01-development-environment/ex01/src/main.js
+++ 02-front-end/section-01-ntroduction-to frontend-and-asynchronous-javascript/day-01-development-environment/ex01/src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!!!!')
+import { nanoid } from 'nanoid';
+
+const passwordBtn = document.querySelector('button');
+const displayPassword = document.querySelector('h2');
+
+
+passwordBtn.addEventListener('click', () => {
+    displayPassword.innerHTML = nanoid()
+})
+
