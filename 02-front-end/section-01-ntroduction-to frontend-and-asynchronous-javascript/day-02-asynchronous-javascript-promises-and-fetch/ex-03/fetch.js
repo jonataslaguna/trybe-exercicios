799c7bd92ef41e35983b6d51ba689c94070f2e80
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-05-22 12:27:21 -0300

    exercise-03/7.2

---
+++ 02-front-end/section-01-ntroduction-to frontend-and-asynchronous-javascript/day-02-asynchronous-javascript-promises-and-fetch/ex-03/fetch.js
@@ -0,0 +1,10 @@
+/* fetch('https://invalid.dummyjson.com/products/27')
+  .then((response) => response.json())
+  .then((data) => console.log('GET sem headers', data))
+  .catch((error) => console.log('Erro ao fazer requisição.', error.message)); */
+
+fetch('https://api.goprogram.ai/inspiration')
+    .then((response) => response.json())
+    .then((data) => console.log(`${data.quote} ${data.author}`))
+    .catch((error) => console.log(`Erro ao fazer a requisição: ${error.message}`))
+
