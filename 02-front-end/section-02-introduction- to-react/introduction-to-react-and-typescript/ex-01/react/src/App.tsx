55a82740d07b1093feefdb7efe3e79ba5d497b17
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-06-01 10:24:47 -0300

    adicionei um button

--- 02-front-end/section-02-introduction- to-react/introduction-to-react-and-typescript/ex-01/react/src/App.tsx
+++ 02-front-end/section-02-introduction- to-react/introduction-to-react-and-typescript/ex-01/react/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from './components/header';
 import Footer from './components/footer';
+import Button from './components/Button';
 
 function App() {
   return (
     <div>
       <Header />
+      <Button />
       <Footer />
+
     </div>
   );
 }
---
+++ 02-front-end/section-02-introduction- to-react/introduction-to-react-and-typescript/ex-01/react/src/components/Button.tsx
@@ -0,0 +1,12 @@
+function Button() {
+  function handleClick() {
+    alert('Meu primeiro event handler com React');
+  }
+  return (
+    <button onClick={ handleClick }>
+      Clique em mim!
+    </button>
+  );
+}
+
+export default Button;
