ea1ba43d00f705e522ed2839fd8d103bce8bae77
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-06-02 13:12:50 -0300

    adicionei um handler genérico

--- 02-front-end/section-02-introduction- to-react/introduction- to-react/my-first-react-project/src/exercises/form/form.tsx
+++ 02-front-end/section-02-introduction- to-react/introduction- to-react/my-first-react-project/src/exercises/form/form.tsx
@@ -3,25 +3,47 @@ import { useState } from "react";
 import './form.css'
 
 function Form() {
-    const [name, setName] = useState('');
+   /*  const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [schooling, setSchooling] = useState('Médio');
-    const [resume, setResume] = useState('');
+    const [resume, setResume] = useState(''); */
     const [terms, setTerms] = useState(false);
     const [error, setError ] = useState(false);
 
+    const [formInfo, setFormInfo] = useState({
+        name: '',
+        email: '',
+        schooling: 'Médio',
+        resume: '',
+    })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const {name, value } = event.target;
+
+       setFormInfo({
+        ...formInfo,
+        [name]: value,
+       });
+    }
+
     const resetForm = () => {
-        setName('');
-        setEmail('');
-        setSchooling('');
-        setResume('');
+        setFormInfo({
+            name: '',
+            email: '',
+            schooling: 'Médio',
+            resume: '',
+          });
     }
 
     const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
       if (terms) {
-        alert(`Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`)
         resetForm();
+        setTerms(false);
+        setError(false);
+        alert(
+          `Nome: ${formInfo.name}\nEmail: ${formInfo.email}\nEscolaridade: ${formInfo.schooling}\nExperiências: ${formInfo.resume}`
+        );
       } else {
         setError(true);
       }
@@ -32,17 +54,17 @@ function Form() {
         <>
             <form  onSubmit={(event) => handleSubmit(event)}>
                 <label htmlFor="inputName">Nome:</label>
-                <input id="inputName" value={ name } onChange={({ target}) => setName(target.value)} required/>
+                <input id="inputName" value={ formInfo.name } name="name" onChange={handleChange} required/>
                 <label htmlFor="inputEmail">Email:</label>
-                <input type="email" id="inputEmail" value={ email } onChange={({ target }) => setEmail(target.value)} required/>
+                <input type="email" id="inputEmail" value={ formInfo.email } name="email" onChange={handleChange} required/>
                 <label htmlFor="schooling">Escolaridade:</label>
-                <select id="schooling" value={ schooling } onChange={({ target }) => setSchooling(target.value)}>
+                <select id="schooling" value={ formInfo.schooling } name="shooling" onChange={handleChange}>
                     <option value="Médio">Médio</option>
                     <option value="Superior">Superior</option>
                     <option value="Pós-graduação">Pós-graduação</option>
                 </select>
                 <label htmlFor="textArea">Resumo das experiências:</label>
-                <textarea id="textArea" value={resume} onChange={({ target }) => setResume(target.value)} required></textarea>
+                <textarea id="textArea" value={formInfo.resume} name="resume" onChange={handleChange} required></textarea>
                 <label htmlFor="terms">
                     Aceito todos os termos e condições
                     <input type="checkbox" id='terms'checked={terms} onChange={() => setTerms((prevTerms) => !prevTerms)}/>
