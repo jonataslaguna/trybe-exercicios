7b4667d9aada02399e26c80d6d17cf6c56fad126
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-06-12 12:39:04 -0300

    add outlet

--- 02-front-end/section-03-going-deeper-in-react/day-01-react-router/exercise/React-Router/src/App.tsx
+++ 02-front-end/section-03-going-deeper-in-react/day-01-react-router/exercise/React-Router/src/App.tsx
@@ -1,19 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './componets/pages/home/Home';
 import CoffeeList from './componets/pages/coffee-list/CoffeeList';
-import NavBar from './componets/nav-bar/NavBar';
 import Coffee from './componets/pages/Coffee';
 import NotFound from './componets/pages/NotFound';
+import Layout from './componets/pages/Layout';
 
 function App() {
 
   return (
     <>
-    <NavBar />
     <Routes> 
-      <Route path='/' element={ <Home /> } />
-      <Route path='coffees' element={ <CoffeeList /> }/>
-      <Route path='/coffees/:coffee' element={ <Coffee />}> </Route>
+      <Route path='/' element={ <Layout />}>
+        <Route index element={ <Home /> } />
+        <Route path='coffees' element={ <CoffeeList /> }/>
+        <Route path='/coffees/:coffee' element={ <Coffee />}> </Route>
+      </Route>
       <Route path="/*" element={ <NotFound /> } />
     </Routes>
     </>
