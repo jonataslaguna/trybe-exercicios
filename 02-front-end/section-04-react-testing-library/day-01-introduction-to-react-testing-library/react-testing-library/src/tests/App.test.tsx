d70be6f042fa5fb66b213ffc01aacc7ec206e346
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-06-21 10:54:33 -0300

    ex-react-testing

--- 02-front-end/section-04-react-testing-library/day-01-introduction-to-react-testing-library/react-testing-library/src/tests/App.test.tsx
+++ 02-front-end/section-04-react-testing-library/day-01-introduction-to-react-testing-library/react-testing-library/src/tests/App.test.tsx
@@ -2,7 +2,32 @@ import { render, screen } from '@testing-library/react';
 
 import App from "../App";
 
-it('should show "Vite + React" text', () => {
+/* it('should show "Vite + React" text', () => {
     render(<App />);
     expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
-})
+}) */
+
+test('Verifica se existe o campo Email.', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
+  
+  test('Verifica se existem dois botões', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+  
+  test('Verifica se existe um botão enviar na tela', () => {
+    render(<App />);
+    const btnSend = screen.getByTestId('id-send');
+    expect(btnSend).toBeInTheDocument();
+  });
+  
+  test('Verifica se existe um botão voltar na tela', () => {
+    render(<App />);
+    const btnBack = screen.getByRole('button', { name: 'Voltar' });
+    expect(btnBack).toBeInTheDocument();
+  });
