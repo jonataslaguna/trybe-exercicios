46d970bd94580e116c1946b019ce2aec66ce1480
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-06-22 19:07:33 -0300

    exercicio com mocks

--- 02-front-end/section-04-react-testing-library/day-02-mocks/exercise-mocks/src/App.tsx
+++ 02-front-end/section-04-react-testing-library/day-02-mocks/exercise-mocks/src/App.tsx
@@ -4,20 +4,29 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [joke, setJoke] = useState('');
 
+  const handleClick = async () => {
+    await fetchJoke();
+  }
+
   useEffect(() => {
-    async function fetchJoke() {
-      const API_URL = 'https://icanhazdadjoke.com/';
-      const REQUEST_CONFIG = { headers: { Accept: 'application/json' } };
-      const response = await fetch(API_URL, REQUEST_CONFIG);
-      const data = await response.json();
-      setJoke(data.joke)
-    }
     fetchJoke();
   }, []);
-
+  
+  async function fetchJoke() {
+    const API_URL = 'https://icanhazdadjoke.com/';
+    const REQUEST_CONFIG = { headers: { Accept: 'application/json' } };
+    const response = await fetch(API_URL, REQUEST_CONFIG);
+    const data = await response.json();
+    setJoke(data.joke)
+  }
   return (
     <div className="App">
       <p>{joke}</p>
+      <button 
+       onClick={ handleClick }
+      >
+      New joke
+      </button>
     </div>
   );
 }
