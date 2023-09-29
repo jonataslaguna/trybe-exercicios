9208f6dab9ae1f97fed49469328dec2bb87d6842
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-09-29 11:15:06 -0300

    more exercises with routes

--- 03-back-end/introduction-to-web -development-with-node/exercises-node/soccer-team-manager/src/app.js
+++ 03-back-end/introduction-to-web -development-with-node/exercises-node/soccer-team-manager/src/app.js
@@ -19,6 +19,20 @@ const teams = [
 
 app.get('/teams', (req, res) => res.status(200).json({ teams })); 
 
+app.get('/teams/:id', (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const teamFound = teams.find((team) => team.id === Number(id));
+    if (!teamFound) {
+      res.status(404).json({ message: 'Team not found' });  
+    }
+    res.status(200).json(teamFound);
+  } catch (error) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+}); 
+
 app.post('/teams', (req, res) => {
     const newTeam = { ...req.body };
     teams.push(newTeam);
@@ -30,12 +44,14 @@ app.put('/teams/:id', (req, res) => {
     const { id } = req.params;
     const { name, initials } = req.body;
   
-    const updateTeam = teams.find((team) => team.id === Number(id));
+    // procurando o elemento que tenha o mesmo id params
+    const updateTeam = teams.find((team) => team.id === Number(id)); 
   
     if (!updateTeam) {
       return res.status(404).json({ message: 'Team not found' });
     }
-  
+
+    // Alterando a propriedade name e initials do elemento que tenha o mesmo id do parametro passado
     updateTeam.name = name;
     updateTeam.initials = initials;
     res.status(200).json({ updateTeam });
@@ -43,10 +59,10 @@ app.put('/teams/:id', (req, res) => {
 
   app.delete('/teams/:id', (req, res) => {
     const { id } = req.params;
-    const arrayPosition = teams.findIndex((team) => team.id === Number(id));
-    teams.splice(arrayPosition, 1);
+    
+    const updadteTeams = teams.filter((team) => team.id !== Number(id));
   
-    res.status(200).end();
+    res.status(200).json(updadteTeams);
   });
 
 module.exports = app;
