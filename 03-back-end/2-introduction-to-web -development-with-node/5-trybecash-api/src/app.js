dffda73a0f2eb4afe64d3a326baf51d175ba5cc0
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-09 10:35:07 -0300

    exercises mysql and node

--- 03-back-end/2-introduction-to-web -development-with-node/5-trybecash-api/src/app.js
+++ 03-back-end/2-introduction-to-web -development-with-node/5-trybecash-api/src/app.js
@@ -13,4 +13,21 @@ app.get('/users', async (req, res) => {
   return res.status(200).json(users);
 })
 
+app.post('/users', async (req, res) => {
+  const { firstName, lastName, email, phone } = req.body;
+
+  const user = {
+    firstName,
+    lastName,
+    email,
+    phone,
+  };
+
+  const dataBaseResponse = await connection.execute(
+   `INSERT INTO people (first_name, last_name, email, phone) 
+   VALUES (?, ?, ?, ?)`, [firstName, lastName, email, phone]);
+
+  return res.status(200).json({id: dataBaseResponse[0].insertId, ...user});
+});
+
 module.exports = app;
