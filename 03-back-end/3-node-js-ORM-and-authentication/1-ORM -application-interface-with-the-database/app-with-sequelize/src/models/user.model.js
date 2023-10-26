500e94ea21e1c5fb7276950bf6951c033df78854
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-26 09:58:40 -0300

    sequelize exercise

---
+++ 03-back-end/3-node-js-ORM-and-authentication/1-ORM -application-interface-with-the-database/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,14 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      fullName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      phoneNum: DataTypes.STRING,
+    },
+    {
+        tableName: 'Users'
+    });
+
+    return User;
+};
+  
+module.exports = UserModel;
