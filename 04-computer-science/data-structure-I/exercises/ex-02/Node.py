ff7c7ae31c8f17431eb9a47342e780a8b8e515f8
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-30 12:10:18 -0300

    linked list

---
+++ 04-computer-science/data-structure-I/exercises/ex-02/Node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, value):
+        self.value = value  # Dado a ser armazenado
+        self.next = None  # Forma de apontar para outro nó
+
+    def __str__(self):
+        return f"Node(value={self.value}, next={self.next})"
