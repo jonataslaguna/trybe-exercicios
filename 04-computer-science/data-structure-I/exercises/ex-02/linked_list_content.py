ac1b0dd9cb384ec2984e72d0965bfd383768d3dd
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-31 10:59:25 -0300

    exercise double linked list

--- 04-computer-science/data-structure-I/exercises/ex-02/linked_list_content.py
+++ 04-computer-science/data-structure-I/exercises/ex-02/linked_list_content.py
@@ -4,6 +4,7 @@ from Node import Node
 class LinkedList:
     def __init__(self):
         self.head_value = None
+        self.tail = None
         self.__length = 0
 
     def __str__(self):
@@ -16,18 +17,22 @@ class LinkedList:
         first_value = Node(value)
         first_value.next = self.head_value
         self.head_value = first_value
+        if self.is_empty():
+            self.tail = self.head_value
         self.__length += 1
 
     def insert_last(self, value):
-        last_value = Node(value)
-        current_value = self.head_value
-
         if self.is_empty():
             return self.insert_first(value)
 
-        while current_value.next:
-            current_value = current_value.next
-        current_value.next = last_value
+        last_value = Node(value)
+        # current_value = self.head_value
+
+        # while current_value.next:
+        #    current_value = current_value.next
+
+        self.tail.next = last_value
+        self.tail = last_value
         self.__length += 1
 
     def insert_at(self, value, position):
@@ -100,7 +105,6 @@ class LinkedList:
         return not self.__length
 
 
-# Para testar, apenas rode o arquivo com: `python3 linked_list_content.py` :)
 if __name__ == "__main__":
     linked_list = LinkedList()
 
